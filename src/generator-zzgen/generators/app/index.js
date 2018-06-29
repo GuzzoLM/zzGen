096ecd835ffcdba16a63f0ce1db2e533ceeec6a5
Lucas Guzzo <lucas.guzzo@radixeng.com.br>
2018-06-29 18:32:05 -0300

    Fix csproj references

--- src/generator-zzgen/generators/app/index.js
+++ src/generator-zzgen/generators/app/index.js
@@ -34,39 +34,95 @@ module.exports = class extends Generator {
 
   writing() {
     this.sourceRoot(path.join(__dirname, '../../templates/aspnetcore'));
+    this._writeDomainModel();
+    this._writeRepositories();
+    this._writeServices();
+    this._writeUseCases();
+    this._writeCore();
+    this._writeHost();
+    if (this.props.createSln) this._writeSolution();
+  }
 
-    this.fs.copy(
-      this.templatePath(this.sourceRoot() + '/project.DomainModel/project.DomainModel.csproj'),
-      this.destinationPath(this.props.appName + '.DomainModel/' + this.props.appName + '.DomainModel.csproj')
-    );
-    this.fs.copy(
-      this.templatePath(this.sourceRoot() + '/project.Repositories/project.Repositories.csproj'),
-      this.destinationPath(this.props.appName + '.Repositories/' + this.props.appName + '.Repositories.csproj')
-    );
+  _writeDomainModel() {
+    var sln = this.fs.read(this.sourceRoot() + '/project.DomainModel/project.DomainModel.csproj');
+    var newSln = sln.split('project.').join(this.props.appName + '.');
+    this.fs.write(this.destinationPath(this.props.appName + '.DomainModel/' + this.props.appName + '.DomainModel.csproj'), newSln);
+  }
+
+  _writeRepositories() {
+    var sln = this.fs.read(this.sourceRoot() + '/project.Repositories/project.Repositories.csproj');
+    var newSln = sln.split('project.').join(this.props.appName + '.');
+    this.fs.write(this.destinationPath(this.props.appName + '.Repositories/' + this.props.appName + '.Repositories.csproj'), newSln);
     mkdirp.sync(this.destinationPath(this.props.appName + '.Repositories/Interfaces/'));
     mkdirp.sync(this.destinationPath(this.props.appName + '.Repositories/Repositories/'));
-    this.fs.copy(
-      this.templatePath(this.sourceRoot() + '/project.Services/project.Services.csproj'),
-      this.destinationPath(this.props.appName + '.Services/' + this.props.appName + '.Services.csproj')
-    );
+  }
+
+  _writeServices() {
+    var sln = this.fs.read(this.sourceRoot() + '/project.Services/project.Services.csproj');
+    var newSln = sln.split('project.').join(this.props.appName + '.');
+    this.fs.write(this.destinationPath(this.props.appName + '.Services/' + this.props.appName + '.Services.csproj'), newSln);
     mkdirp.sync(this.destinationPath(this.props.appName + '.Services/Interfaces/'));
     mkdirp.sync(this.destinationPath(this.props.appName + '.Services/Services/'));
-    this.fs.copy(
-      this.templatePath(this.sourceRoot() + '/project.UseCases/project.UseCases.csproj'),
-      this.destinationPath(this.props.appName + '.UseCases/' + this.props.appName + '.UseCases.csproj')
-    );
+  }
+
+  _writeUseCases() {
+    var sln = this.fs.read(this.sourceRoot() + '/project.UseCases/project.UseCases.csproj');
+    var newSln = sln.split('project.').join(this.props.appName + '.');
+    this.fs.write(this.destinationPath(this.props.appName + '.UseCases/' + this.props.appName + '.UseCases.csproj'), newSln);
     mkdirp.sync(this.destinationPath(this.props.appName + '.UseCases/Interfaces/'));
     mkdirp.sync(this.destinationPath(this.props.appName + '.UseCases/UseCases/'));
-    this.fs.copy(
-      this.templatePath(this.sourceRoot() + '/project.Web.Core/project.Web.Core.csproj'),
-      this.destinationPath(this.props.appName + '.Web.Core/' + this.props.appName + '.Web.Core.csproj')
-    );
+  }
+
+  _writeCore() {
+    var sln = this.fs.read(this.sourceRoot() + '/project.Web.Core/project.Web.Core.csproj');
+    var newSln = sln.split('project.').join(this.props.appName + '.');
+    this.fs.write(this.destinationPath(this.props.appName + '.Web.Core/' + this.props.appName + '.Web.Core.csproj'), newSln);
     mkdirp.sync(this.destinationPath(this.props.appName + '.Web.Core/Authentication/'));
+  }
+
+  _writeHost(){
+    var sln = this.fs.read(this.sourceRoot() + '/project.Web.Host/project.Web.Host.csproj');
+    var newSln = sln.split('project.').join(this.props.appName + '.');
+    this.fs.write(this.destinationPath(this.props.appName + '.Web.Host/' + this.props.appName + '.Web.Host.csproj'), newSln);
     this.fs.copy(
-      this.templatePath(this.sourceRoot() + '/project.Web.Host/project.Web.Host.csproj'),
-      this.destinationPath(this.props.appName + '.Web.Host/' + this.props.appName + '.Web.Host.csproj')
+      this.templatePath(this.sourceRoot() + '/project.Web.Host/Controllers/'),
+      this.destinationPath(this.props.appName + '.Web.Host/Controllers/')
     );
-    if (this.props.createSln) this._writeSolution();
+    this.fs.copy(
+      this.templatePath(this.sourceRoot() + '/project.Web.Host/Models/'),
+      this.destinationPath(this.props.appName + '.Web.Host/Models/')
+    );
+    this.fs.copy(
+      this.templatePath(this.sourceRoot() + '/project.Web.Host/Properties/'),
+      this.destinationPath(this.props.appName + '.Web.Host/Properties/')
+    );
+    this.fs.copy(
+      this.templatePath(this.sourceRoot() + '/project.Web.Host/Views/'),
+      this.destinationPath(this.props.appName + '.Web.Host/Views/')
+    );
+    this.fs.copy(
+      this.templatePath(this.sourceRoot() + '/project.Web.Host/wwwroot/'),
+      this.destinationPath(this.props.appName + '.Web.Host/wwwroot/')
+    );
+    this.fs.copy(
+      this.templatePath(this.sourceRoot() + '/project.Web.Host/appsettings.Development.json'),
+      this.destinationPath(this.props.appName + '.Web.Host/appsettings.Development.json')
+    );
+    this.fs.copy(
+      this.templatePath(this.sourceRoot() + '/project.Web.Host/appsettings.json'),
+      this.destinationPath(this.props.appName + '.Web.Host/appsettings.json')
+    );
+    this.fs.copy(
+      this.templatePath(this.sourceRoot() + '/project.Web.Host/Program.cs'),
+      this.destinationPath(this.props.appName + '.Web.Host/Program.cs')
+    );
+    this.fs.copy(
+      this.templatePath(this.sourceRoot() + '/project.Web.Host/Startup.cs'),
+      this.destinationPath(this.props.appName + '.Web.Host/Startup.cs')
+    );
+    var sln = this.fs.read(this.destinationPath(this.props.appName + '.Web.Host/Views/Shared/_Layout.cshtml'));
+    var newSln = sln.split('projectName').join(this.props.appName);
+    this.fs.write(this.destinationPath(this.props.appName + '.Web.Host/Views/Shared/_Layout.cshtml'), newSln);
   }
 
   _writeSolution() {
